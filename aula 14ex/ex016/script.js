5077802abb8c4844558e93a992dd16275f0faa70
Arthur Cordeiro <agstcordeiro@gmail.com>
2022-12-19 11:52:15 -0300

    atualização nos códigos

--- aula 14ex/ex016/script.js
+++ aula 14ex/ex016/script.js
@@ -1,82 +1,60 @@
 //Puxando os elementos com DOM, para conseguir fazer as modificações
 
 function contar(){
+    //declarando variaveis
+    let ini = document.getElementById("txti")
+    let fim = document.getElementById("txtf")
+    let passo = document.getElementById("txtp")
+    let res = document.getElementById("res")
 
-    //Puxando os elementos
-    var varInit = document.getElementById('varInit')
-    var varFin = document.getElementById('varFin')
-    var varCont = document.getElementById('varCont')
-
-    //converter para numero
-    varInit = Number(varInit.value)
-    varFin = Number(varFin.value)
-    varCont = Number(varCont.value)
-
-    if(varInit.value.length == 0){
-        window.alert('[Erro] Verifique os dados e tente novamente!')
-    } else if(varFin.value.length == 0){
-        window.alert('[Erro] Verifique os dados e tente novamente!')
-    } else if(varCont.value.length == 0){
-        window.alert('[Erro] Verifique os dados e tente novamente!')
-    } else if(varCont == 0){
-        window.alert(`Passo inválido. Considerando PASSO 1!`)
-        varCont = 1 
-            for(var i = varInit; i <= varFin; i += varCont){
-                res.innerHTML += (`${i} ,`)
-                }
-    } else {
-        for(var i = varInit; i <= varFin; i += varCont){
-            res.innerHTML += (`${i} ,`)
+    //Validação de dados
+    if (ini.value.length == 0 || fim.value.length  == 0 || passo.value.length == 0){
+        res.innerHTML = 'Impossível contar!'
+        window.alert('[Erro] Faltam dados!')
+    }else{
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if(i < f){
+            //Contagem crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
             }
+        } else { 
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            //Contagem regressiva
+        }
+
+        for(let c = i; c <= f; c += p){
+            res.innerHTML += `${c} \u{1F449}`
+        }
+        res.innerHTML +=`\u{1F3C1}`
     }
-
-
-
-
-
-
-    
-
 
 
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 /*
-    var txt1 = document.getElementById('txt1')
-    var txt2 = document.getElementById('txt2')
-    var txt3 = document.getElementById('txt3')
-    var res = document.getElementById('res')
-
-
-
-        //ver resolução para corrigir validação
-    if(varInit == 0){
-        res.innerHTML = (`Impossível contar!`)
-    }
-     else if(varFin == 0){
-        res.innerHTML = (`Impossível contar!`)
-    }
-     else if(varCont == 0){
-        res.innerHTML = (`Impossível contar!`)
-    }
-     else if(varCont == 0){
-        window.alert(`Passo inválido. Considerando PASSO 1`)
-        varCont = 1
-        for(var i = varInit; i <= varFin; i += varCont){
-            res.innerHTML += (`${i}, `)
-        }
-        res.innerHTML +=(` <strong>Fim!<strong>`)
-
-    } else {
-        for(var i = varInit; i <= varFin; i += varCont){
-            res.innerHTML += (`${i}, `)
-        }
-        res.innerHTML +=(` <strong>Fim!<strong>`)
-    }
-
-
-
 var agora = new Date()
 var horas = agora.getHours()
 Declarando variaveis
